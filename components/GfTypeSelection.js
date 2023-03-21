cacedde6524604cf23f6adb5b85c52385fc00310
Téo <gravelteo.pro@gmail.com>
2023-03-21 17:17:54 +0100

    fx #10

---
+++ components/GfTypeSelection.js
@@ -0,0 +1,8 @@
+import { html, css } from "./GfBase.js";
+import GfInput from "./GfInput.js";
+
+export default class GfTypeSelection extends GfInput {
+    constructor() {
+        super();
+    }
+}
